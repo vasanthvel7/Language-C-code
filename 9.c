c74d1d06896f70c2f0f6d8742636a9bc02a99320
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2018-12-20 14:31:54 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -2,21 +2,22 @@
 #include<conio.h>
 void main()
 {
-int N,K,n[5],i;
+int N,K,n[5],i,sum;
 printf("Enter the no of values:");
-scanf("%d",N);
+scanf("%d",&N);
 printf("Enter the no of values:");
-scanf("%d",K);
+scanf("%d",&K);
 printf("Enter the values:");
 for(i=1;i<=5;i++)
 {
 scanf("%d",&n[i]);
 }
-printf("Enter the values:");
+
 for(i=0;i<=K;i++)
 {
 sum=sum+i;
 }
+printf("sum of K:%d",sum);
 }
 
 
