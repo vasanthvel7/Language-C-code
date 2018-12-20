fd4df496679d3011f05983f6f64c6268c3c2854a
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2018-12-20 13:58:08 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int N,K,n[5],i;
+printf("Enter the no of values:");
+scanf("%d",N);
+printf("Enter the no of values:");
+scanf("%d",K);
+printf("Enter the values:");
+for(i=1;i<=5;i++)
+{
+scanf("%d",&n[i]);
+}
+printf("Enter the values:");
+for(i=0;i<=K;i++)
+{
+sum=sum+i;
+}
+}
+
+
