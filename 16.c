f25a41133e55591d41fcb49998cdbe826e42e1f4
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2019-02-05 12:40:11 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int low, high, i, flag;
+   scanf("%d %d", &low, &high);
+    while (low < high)
+    {
+        flag = 0;
+
+        for(i = 1; i <= low/2; ++i)
+        {
+            if(low % i == 0)
+            {
+                flag = i;
+                break;
+            }
+        }
+
+        if (flag == 0)
+         if(low>i)
+            printf("%d ", low);
+
+        ++low;
+    }
+
+    return 0;
+}
