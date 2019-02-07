ae21aa5d84afa1034dc1644f31499fe210142ea0
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2019-02-07 14:37:10 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -4,10 +4,12 @@ void main()
     int n,n1=1,i;
     scanf("%d",&n);
     {
-        for(i=1;i<=n;i++)
+        for(i=1;i<=5;i++)
         {
           n1=i*n;
           printf("%d\t",n1);
+          n1++;
         }
     }
 }
+
