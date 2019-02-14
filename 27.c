bcbe2eb2908e4f4fcee09548b0cdb0c9e1be6aa5
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2019-02-14 05:38:03 -0800

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+  
+int main() 
+{ 
+    char str[5],i; 
+          scanf("%c",&str[i]);
+         if (isdigit(str[i]) != 0) 
+         {
+             printf("yes");
+         }
+         else
+         {
+             printf("no");
+         }
+  
+      return 0; 
+} 
