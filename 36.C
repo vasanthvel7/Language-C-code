f68a31f5d4e5a43859707262a8a5081cd4b3d6aa
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2019-02-18 05:11:20 -0800

    Create 36.C

---
+++ 36.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char s[1000];
+    int c=0,j;
+    gets(s);
+    for(j = 0; s[j] != '\0'; j++)
+    {
+        if(s[j]=='.' || s[j]==','|| s[j]=='$'|| 
+        s[j]=='@'|| s[j]==')'|| s[j]=='('|| 
+        s[j]=='#'|| s[j]=='^'|| s[j]=='*'|| s[j]=='+')
+        c++;
+    }
+    printf("%d", c);
+    return 0;
+}
