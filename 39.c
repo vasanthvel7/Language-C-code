eab658450dd9c9c4ded927bf6e7db31a70a10c16
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2019-02-18 05:20:44 -0800

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int a[10],max,i;
+for(i=0;i<9;i++)
+{
+    scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<9;i++)
+{
+    if(max<a[i])
+    {
+        max=a[i];
+    }
+}
+printf("%d",max);
+}
