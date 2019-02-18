b75cc670175bebe760f25532d3f2a43f275988d6
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2019-02-18 04:55:04 -0800

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+void swap(int*, int *);
+void main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    swap(&num1, &num2);       
+    printf("%d %d", num1, num2);
+}
+void swap(int *x, int *y)
+{
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+}
