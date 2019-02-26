6e505065f8dffed7701abe737c6a2fe7127f8865
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2019-02-26 12:11:09 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,a[10],i,count=0;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<4;i++)
+    {
+        if(a[i]==k)
+        {
+            count=count+1;
+        }
+        
+    }
+if(count!=0)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+}
