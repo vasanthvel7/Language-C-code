9ea81b796952df7e0525ae8e45b5e9b61ca2a283
vasanthvel7 <43337490+vasanthvel7@users.noreply.github.com>
2019-03-05 12:11:17 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include<ctype.h>
+
+int main()
+{
+    int i;
+    int c1,n,count=0,count1=0;
+	char c[100];
+	gets(c);
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    printf("%c",c[i]);
+	}
+	
+
+	
+}
